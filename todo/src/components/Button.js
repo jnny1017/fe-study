d1de7e0e3efdd909db2f1f5d25ac4507189df90b
jnny1017 <jnny1017@gmail.com>
2023-07-29 08:01:50 +0900

    fixed untracked files

--- todo/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-export default function Button({ text, onClick }) {
-  return (
-    <button type='button' onClick={onClick}>
-      {text}
-    </button>
-  );
-}
--- todo/src/components/Checkbox.js
+++
@@ -1,8 +0,0 @@
-export default function TodoList({ id, children, onChange }) {
-  return (
-    <label htmlFor={id}>
-      <input type='checkbox' id={id} onChange={onChange} />
-      {children}
-    </label>
-  );
-}
